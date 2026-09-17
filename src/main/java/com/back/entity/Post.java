81ad7af2011c7704c5efe57ce6de134568bec037
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-17 12:21:36 +0900

    0003 commit

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -1,12 +1,15 @@
 package com.back.entity;
 
 import com.back.jpa.entity.BaseIdAndTime;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,10 +19,24 @@ public class Post extends BaseIdAndTime {
     private String title;
     @Column(columnDefinition = "LONGTEXT")
     private String content;
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
 
     public Post(Member author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }
