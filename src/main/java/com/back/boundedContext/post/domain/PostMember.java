ea1b1965a3251f327702dfa02e1a088aa4d88964
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-17 15:30:31 +0900

    0016 commit

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,6 +1,6 @@
 package com.back.boundedContext.post.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.global.jpa.entity.BaseIdAndTimeManual;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "POST_MEMBER")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PostMember extends BaseIdAndTime {
+public class PostMember extends BaseIdAndTimeManual {
     @Column(unique = true)
     private String username;
     private String password;
