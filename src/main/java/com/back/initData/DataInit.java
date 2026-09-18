ded4209049d8d6290e93898c0d93716b9dcb6d07
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-18 12:19:11 +0900

    Post 생성

--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -1,7 +1,10 @@
 package com.back.initData;
 
-import com.back.service.MemberService;
 import com.back.entity.Member;
+import com.back.entity.Post;
+import com.back.exception.DomainException;
+import com.back.service.MemberService;
+import com.back.service.PostService;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
@@ -9,21 +12,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
-@Configuration
 @Slf4j
+@Configuration
 public class DataInit {
     private final DataInit self;
     private final MemberService memberService;
+    private final PostService postService;
 
-    public DataInit(@Lazy DataInit self, MemberService memberService) {
+    public DataInit(
+            @Lazy DataInit self,
+            MemberService memberService,
+            PostService postService
+    ) {
         this.self = self;
         this.memberService = memberService;
+        this.postService = postService;
     }
 
     @Bean
     public ApplicationRunner baseInitDataRunner() {
         return args -> {
             self.makeBaseMembers();
+            self.makeBasePosts();
         };
     }
 
@@ -37,5 +47,26 @@ public class DataInit {
         Member user1Member = memberService.join("user1", "1234", "유저1");
         Member user2Member = memberService.join("user2", "1234", "유저2");
         Member user3Member = memberService.join("user3", "1234", "유저3");
+    }
+
+    @Transactional
+    public void makeBasePosts() {
+        if (postService.count() > 0) return;
+
+        Member user1 = getMemberOrThrow("user1");
+        Member user2 = getMemberOrThrow("user2");
+        Member user3 = getMemberOrThrow("user3");
+
+        Post post1 = postService.write(user1, "제목1", "내용1");
+        Post post2 = postService.write(user1, "제목2", "내용2");
+        Post post3 = postService.write(user1, "제목3", "내용3");
+        Post post4 = postService.write(user2, "제목4", "내용4");
+        Post post5 = postService.write(user2, "제목5", "내용5");
+        Post post6 = postService.write(user3, "제목6", "내용6");
+    }
+
+    private Member getMemberOrThrow(String username) {
+        return memberService.findByUsername(username)
+                .orElseThrow(() -> new DomainException("401-1", "%s을 찾을 수 없습니다.".formatted(username)));
     }
 }
