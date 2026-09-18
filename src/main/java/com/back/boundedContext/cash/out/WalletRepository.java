51502b57a9163ba0a9cfa836d5c7288b8a1c369a
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-18 17:22:17 +0900

    0025 commit

--- src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
+++ src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
@@ -1,7 +1,11 @@
 package com.back.boundedContext.cash.out;
 
+import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface WalletRepository extends CrudRepository<Wallet, Integer> {
+    Optional<Wallet> findByHolder(CashMember holder);
 }
