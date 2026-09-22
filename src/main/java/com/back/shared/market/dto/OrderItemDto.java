9c10647a278638ff90b63e685c8ef617bba7b1bd
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-22 16:15:35 +0900

    0044 commit

--- src/main/java/com/back/shared/market/dto/OrderItemDto.java
+++ src/main/java/com/back/shared/market/dto/OrderItemDto.java
@@ -1,5 +1,7 @@
 package com.back.shared.market.dto;
 
+import com.back.shared.modelType.HashModelTypeCode;
+
 import java.time.LocalDateTime;
 
 public record OrderItemDto(
@@ -15,6 +17,12 @@ public record OrderItemDto(
         String productName,
         long price,
         long salePrice,
-        double payoutRate
-) {
+        double payoutRate,
+        long payoutFee,
+        long salePriceWithoutFee
+) implements HashModelTypeCode {
+    @Override
+    public String getModelTypeCode() {
+        return "OrderItem";
+    }
 }
