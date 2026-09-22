4cb95e3737303dcbcfaf307d050517e6aa49b69c
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-22 11:33:19 +0900

    0038 commit

--- src/main/java/com/back/boundedContext/market/in/MarketEventListener.java
+++ src/main/java/com/back/boundedContext/market/in/MarketEventListener.java
@@ -40,12 +40,12 @@ public class MarketEventListener {
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(CashOrderPaymentSucceededEvent event) {
-        marketFacade.handle(event);
+        marketFacade.completeOrderPayment(event.order().id());
     }
 
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(CashOrderPaymentFailedEvent event) {
-        marketFacade.handle(event);
+        marketFacade.cancelOrderRequestPayment(event.order().id());
     }
 }
