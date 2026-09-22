00a7ac297b4b628e3bb73f03391ee6af861e9381
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-22 11:14:12 +0900

    0037 commit

--- src/main/java/com/back/shared/market/out/TossPaymentsService.java
+++ src/main/java/com/back/shared/market/out/TossPaymentsService.java
@@ -1,7 +1,5 @@
 package com.back.shared.market.out;
 
-import org.springframework.transaction.annotation.Transactional;
-import tools.jackson.core.type.TypeReference;
 import com.back.global.exception.DomainException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -9,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.nio.charset.StandardCharsets;
